ca0eb97468ff8a767ddcc791494836425552d72e
aybanda <ajaybandaru17@gmail.com>
2024-09-18 22:23:56 +0530

    Add display options for Price component

--- src/5_shared/ui/Price/Price.tsx
+++ src/5_shared/ui/Price/Price.tsx
@@ -4,10 +4,22 @@ import s from './Price.module.css'
 import { FC } from 'react'
 
 const Price: FC<{ amount?: number }> = ({ amount }) => {
+    const displayOption = process.env.REACT_APP_DISPLAY_OPTION;
+
+    const formatAmount = () => {
+        if (displayOption === 'short') {
+            return amount ? `${(amount / 1000).toFixed(0)}k` : '';
+        } else if (displayOption === 'text') {
+            return amount ? `${amount.toLocaleString()} sats` : '';
+        }
+        return amount?.toLocaleString(); // Default case
+    };
+
     return (
         <Flex align="center" >
-            <Typography className={s.value}>{amount}</Typography>
-            <SatsIcon />
+            {/* Format the amount based on the display option */}
+            <Typography className={s.value}>{formatAmount()}</Typography>
+            {displayOption !== 'text' && <SatsIcon />}
         </Flex>
     )
 }
