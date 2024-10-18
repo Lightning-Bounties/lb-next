2c843499a1caedf1eb72b8cffa2ef7775797f61c
sutt <wsutton17@gmail.com>
2024-10-18 19:20:21 -0400

    revert linting changes

--- src/2_widgets/issue/issueBaseInfo/ui/IssueBaseInfo.tsx
+++ src/2_widgets/issue/issueBaseInfo/ui/IssueBaseInfo.tsx
@@ -1,46 +1,51 @@
-import { Breadcrumb, Flex, Tooltip, Typography } from "antd";
-import Title from "antd/es/typography/Title";
-import Link from "next/link";
-import { LinkOutlined, TrophyFilled } from "@ant-design/icons";
-import { Avatar } from "@/5_shared/ui/Avatar/Avatar";
-import { Price } from "@/5_shared/ui/Price/Price";
-import s from "./IssueBaseInfo.module.css";
-import { FC } from "react";
-import { issueApi } from "@/4_entities/issue";
-import { getStringDate } from "@/5_shared/utils/getStringDate";
-import { IssueBreadcrumbs } from "@/3_features/issue";
-import dynamic from "next/dynamic";
-import { appRoutes } from "@/5_shared/config/appRoutes";
-import { IssueBaseInfoCheckPull } from "./IssueBaseInfoCheckPull";
-import { orange, grey } from "@ant-design/colors";
+import { Breadcrumb, Flex, Tooltip, Typography } from 'antd'
+import Title from 'antd/es/typography/Title'
+import Link from 'next/link'
+import { LinkOutlined, TrophyFilled } from '@ant-design/icons'
+import { Avatar } from '@/5_shared/ui/Avatar/Avatar'
+import { Price } from '@/5_shared/ui/Price/Price'
+import s from './IssueBaseInfo.module.css'
+import { FC } from 'react'
+import { issueApi } from '@/4_entities/issue'
+import { getStringDate } from '@/5_shared/utils/getStringDate'
+import { IssueBreadcrumbs } from '@/3_features/issue'
+import dynamic from 'next/dynamic'
+import { appRoutes } from '@/5_shared/config/appRoutes'
+import { IssueBaseInfoCheckPull } from './IssueBaseInfoCheckPull'
+import { orange, grey } from '@ant-design/colors';
 
-const IssueBaseInfoDesc = dynamic(() => import("./IssueBaseInfoDesc"), {
-    ssr: false,
-});
+const IssueBaseInfoDesc = dynamic(() => import('./IssueBaseInfoDesc'), { ssr: false })
+
 
 type IssueBaseInfoProps = {
-    rewardId: string;
-};
+    rewardId: string
+}
 
 const IssueBaseInfo: FC<IssueBaseInfoProps> = async ({ rewardId }) => {
+
     try {
-        const data = await issueApi.getIssueData(rewardId);
+        const data = await issueApi.getIssueData(rewardId)
 
         return (
             <Flex vertical gap="large">
                 <div>
                     <IssueBreadcrumbs title={data.repository_data.full_name} />
-                    <Title style={{ display: "inline" }} level={1}>
+                    <Title
+                        style={{ display: 'inline' }}
+                        level={1}
+                    >
                         {data.title}
                     </Title>
                     &nbsp;&nbsp;&nbsp;
-                    <span className={`${s.title__number} opacity50`}>
-            #{data.issue_number}
-          </span>
+                    <span className={`${s.title__number} opacity50`}>#{data.issue_number}</span>
                     &nbsp;
                     <Tooltip title="View on GitHub">
-                        <Link href={data.html_url ?? ""} target="_blank" rel="nofollow">
-                            <LinkOutlined style={{ fontSize: "22px" }} />
+                        <Link
+                            href={data.html_url ?? ''}
+                            target='_blank'
+                            rel="nofollow"
+                        >
+                            <LinkOutlined style={{ fontSize: '22px' }} />
                         </Link>
                     </Tooltip>
                     {data.is_closed && (
@@ -68,10 +73,8 @@ const IssueBaseInfo: FC<IssueBaseInfoProps> = async ({ rewardId }) => {
                     <Flex gap="large" align="center">
                         <Link href={`/${appRoutes.profile}/${data.last_rewarder_data?.id}`}>
                             <Flex align="center" gap="small">
-                                <Avatar avatarUrl={data.last_rewarder_data?.avatar_url ?? ""} />
-                                <Typography>
-                                    {data.last_rewarder_data?.github_username}
-                                </Typography>
+                                <Avatar avatarUrl={data.last_rewarder_data?.avatar_url ?? ''} />
+                                <Typography>{data.last_rewarder_data?.github_username}</Typography>
                             </Flex>
                         </Link>
                     </Flex>
@@ -119,10 +122,10 @@ const IssueBaseInfo: FC<IssueBaseInfoProps> = async ({ rewardId }) => {
                     </Flex>
                 ) : null}
             </Flex>
-        );
+        )
     } catch (e) {
-        return <>Error</>;
+        return <>Error</>
     }
-};
+}
 
 export { IssueBaseInfo }
--- src/2_widgets/issue/issueBaseInfo/ui/IssueBaseInfoDesc.tsx
+++ src/2_widgets/issue/issueBaseInfo/ui/IssueBaseInfoDesc.tsx
@@ -1,21 +1,22 @@
 'use client'
 
-import { FC } from "react";
-import MarkdownPreview from "@uiw/react-markdown-preview";
-import { useUserStore } from "@/4_entities/user";
-import { useShallow } from "zustand/react/shallow";
+import { FC } from 'react'
+import MarkdownPreview from '@uiw/react-markdown-preview';
+import { useUserStore } from '@/4_entities/user';
+import { useShallow } from 'zustand/react/shallow';
 
 const IssueBaseInfoDesc: FC<{ data: string }> = ({ data }) => {
-    const darkTheme = useUserStore(useShallow((state) => state.darkTheme));
+
+    const darkTheme = useUserStore(useShallow(state => state.darkTheme))
     return (
         <MarkdownPreview
-            source={data ?? ""}
+            source={data ?? ''}
             style={{ padding: 16, borderRadius: 9 }}
             wrapperElement={{
-                "data-color-mode": darkTheme ? "dark" : "light",
+                "data-color-mode": darkTheme ? 'dark' : 'light'
             }}
         />
-    );
-};
+    )
+}
 
 export default IssueBaseInfoDesc
