98a8314321d6fd87847a0e3d87153516e7620c63
sutt <wsutton17@gmail.com>
2025-02-01 09:33:08 -0500

    adding more striking color to Select styling

--- src/5_shared/themes/light.ts
+++ src/5_shared/themes/light.ts
@@ -17,7 +17,9 @@ const lightTheme: ThemeConfig = {
             headerPadding: '0 20px'
         },
         Select: {
-            colorTextPlaceholder: "rgb(0, 0, 0)"
+            colorTextPlaceholder: "rgb(0, 0, 0)",
+            controlItemBgHover: "rgba(89, 126, 247, 0.3)",
+            controlItemBgActive: "rgb(89, 126, 247)"
         },
     }
 }
